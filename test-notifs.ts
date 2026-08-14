6e3051764079b014ea3a813f1ae50e3ff468f83a
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2026-08-14 16:20:07 +0700

    chore: remove obsolete test and utility scripts

--- test-notifs.ts
+++
@@ -1,7 +0,0 @@
-import prisma from './src/lib/prisma';
-
-async function main() {
-  const notifications = await prisma.notification.findMany();
-  console.log('Notifications:', notifications);
-}
-main().catch(console.error);
