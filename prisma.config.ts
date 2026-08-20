d563e805262ef4bc2821682066ab7d558b93b334
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2026-08-20 18:08:34 +0700

    fix: fallback DATABASE_URL for prisma generate and remove .next file locking

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 config({ path: ['.env.production', '.env.development', '.env'] });
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -9,6 +9,6 @@ export default defineConfig({
     seed: 'npx tsx prisma/seed.ts',
   },
   datasource: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL || env('DATABASE_URL'),
+    url: process.env.DATABASE_URL || process.env.DIRECT_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 })
